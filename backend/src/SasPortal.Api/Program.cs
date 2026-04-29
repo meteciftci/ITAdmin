b762327c2d1ce116c0b62a917f1b4c0ae5faf567
Mete <e.meteciftci@gmail.com>
2026-04-29 13:19:10 +0300

    feat: configure serilog logging

--- backend/src/SasPortal.Api/Program.cs
+++ backend/src/SasPortal.Api/Program.cs
@@ -2,34 +2,52 @@ using SasPortal.Api.Extensions;
 using SasPortal.Application;
 using SasPortal.Infrastructure;
 using SasPortal.Persistence;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddPersistence(builder.Configuration);
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddHealthChecks();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseGlobalExceptionHandling();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddPersistence(builder.Configuration);
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+    builder.Services.AddHealthChecks();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    app.UseGlobalExceptionHandling();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+    app.MapHealthChecks("/health");
+
+    Log.Information("Starting SAS Portal API");
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-app.MapHealthChecks("/health");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "SAS Portal API terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
