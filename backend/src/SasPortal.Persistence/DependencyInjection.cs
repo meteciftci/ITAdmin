2687b8512b7e1ddc4010bb0f9f25c6baaa07e602
Mete <e.meteciftci@gmail.com>
2026-04-29 13:04:36 +0300

    feat: configure persistence context

--- backend/src/SasPortal.Persistence/DependencyInjection.cs
+++ backend/src/SasPortal.Persistence/DependencyInjection.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SasPortal.Persistence.Context;
 
 namespace SasPortal.Persistence;
 
@@ -7,6 +9,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        }
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
         return services;
     }
 }
