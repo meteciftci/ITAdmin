00f60e5cb8131de4f922809446de082492f8dc23
Mete <e.meteciftci@gmail.com>
2026-04-29 13:12:35 +0300

    feat: add health check endpoint

--- backend/src/SasPortal.Api/Program.cs
+++ backend/src/SasPortal.Api/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddPersistence(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -29,5 +30,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
